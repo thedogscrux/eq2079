3c0b133366e313ed0017b0f34c27f6126b1c758f
Alex Miller <amiller@launchcg.com>
2017-10-19 14:49:15 -0700

    Adding redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+import rootReducer from './reducers'
+
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
